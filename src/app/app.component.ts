6a8921c4e5bbb5507adbc771afd3019719e2a878
Jared <jaredcol13@gmail.com>
2018-02-12 10:22:27 -0700

    Home page initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,11 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      // { title: 'Login', component: LoginPage },
+      // { title: 'Details', component: DetailsPage }
+
+      // need these pages for menu
     ];
 
   }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+// import {startAutoplay} from 'ionic-angular/components/slides/swiper/swiper';
+
+
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,50 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  title = "Movie Reviews";
+  posters = [
+
+    {
+      image: 'assets/imgs/hobbit.jpg'
+    },
+    {
+      image: 'assets/imgs/junglebook.jpg'
+    },
+    {
+      image: 'assets/imgs/ozpic.jpg'
+    }
+  ];
+  popular = [
+    {
+      name: 'Movie title'
+    },
+    {
+      name: 'Movie crap'
+    },
+    {
+      name: 'Movie junk'
+    },
+    {
+      name: 'Movie garbage'
+    },
+    {
+      name: 'Movie crud'
+    },
+    {
+      name: 'Movie words'
+    }
+  ];
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  ionViewDidLoad() {
+      console.log(this.title);
+  }
+
+  startAutoplay() {
+
+  }
 }
+

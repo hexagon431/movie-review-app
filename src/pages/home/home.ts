90ef57ced8fdca80a3c69bfa167484b004f56283
Jared <jaredcol13@gmail.com>
2018-02-15 09:48:20 -0700

    API WORKING

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { MovieApiProvider } from '../../providers/movie-api/movie-api';
+import { Observable } from 'rxjs/Observable';
 
 
 
@@ -14,9 +15,9 @@ export class HomePage {
 
 
   upImage = 'https://image.tmdb.org/t/p/w500/nk11pvocdb5zbFhX5oq5YiLPYMo.jpg';
-  apiKey = '74cff56e7a570daac9b5d7fae1093dc0';
+  // apiKey = '74cff56e7a570daac9b5d7fae1093dc0';
 
-  private movieCoverBaseUrl: string = 'https://image.tmdb.org/t/p/w500';
+  // private movieCoverBaseUrl: string = 'https://image.tmdb.org/t/p/w500';
   title = "Movie Reviews";
   posters = [
     {
@@ -29,36 +30,49 @@ export class HomePage {
       image: 'assets/imgs/hobbit.jpg'
     }
   ];
-  popular = [
-    {
-      name: 'Movie title'
-    },
-    {
-      name: 'Movie crap'
-    },
-    {
-      name: 'Movie junk'
-    },
-    {
-      name: 'Movie garbage'
-    },
-    {
-      name: 'Movie crud'
-    },
-    {
-      name: 'Movie words'
-    }
-  ];
+  disney: Observable<any>;
+  popular: Observable<any>;
+  action: Observable<any>;
   loaded: boolean = false;
 
-  constructor(public navCtrl: NavController, private movie: MovieApiProvider) {
-
-  }
+  constructor(public navCtrl: NavController,
+              public movie: MovieApiProvider) { }
 
   ionViewDidLoad() {
       console.log(this.title);
-      this.movie.getTestMovie();
-   //   this.loaded = true;
+      this.loaded = true;
+
+
+      this.movie.getPopularMovies().subscribe(
+        popular => this.popular = popular.results.sort(function compare(a, b) {
+          if (a.vote_count > b.vote_count) {
+            return -1;
+          }
+          if (a.vote_count < b.vote_count) {
+            return 1;
+          }
+          return 0;
+        })
+      );
+
+
+      this.movie.getMoviesByCompany('2', '2013').subscribe(
+        disney => this.disney = disney.results);
+
+
+
+      this.movie.getMoviesByGenre('28', '2017').subscribe(
+        action => this.action = action.results
+        .sort(function compare(a, b) {
+          if (a.vote_average > b.vote_average) {
+            return -1;
+          }
+          if (a.vote_average < b.vote_average) {
+            return 1;
+          }
+          return 0;
+        })
+        );
   }
 
 
--- src/providers/movie-api/movie-api.ts
+++ src/providers/movie-api/movie-api.ts
@@ -2,80 +2,81 @@ import {Injectable} from '@angular/core';
 import {Observable} from "rxjs/Observable";
 import {HttpClient} from "@angular/common/http";
 import {Movie} from "../../interfaces/Movie";
+import 'rxjs';
 
 @Injectable()
 export class MovieApiProvider {
 
 
-  baseUrl: string = 'https://image.tmdb.org/t/p/w500';
+  movieSearchBaseUrl: string = 'https://api.themoviedb.org/3/movie/';
+  discoverMoviesBaseUrl: string = 'https://api.themoviedb.org/3/discover/movie';
+  moviePosterBaseUrl: string = 'https://image.tmdb.org/t/p/w500';
+  apiKey = "?api_key=74cff56e7a570daac9b5d7fae1093dc0";
+  movies: any = {};
 
-  testMovie: Movie = {
-    "adult": false,
-    "backdrop_path": "/owByaQiAlauEZy6hLd6dwsY4QHf.jpg",
-    "belongs_to_collection": null,
-    "budget": 175000000,
-    "genres": [
-      {
-        "id": 16,
-        "name": "Animation"
-      },
-      {
-        "id": 35,
-        "name": "Comedy"
-      },
-      {
-        "id": 10751,
-        "name": "Family"
-      },
-      {
-        "id": 12,
-        "name": "Adventure"
-      }
-    ],
-    "homepage": "http://disney.go.com/disneypictures/up/",
-    "id": 14160,
-    "imdb_id": "tt1049413",
-    "original_language": "en",
-    "original_title": "Up",
-    "overview": "Carl Fredricksen spent his entire life dreaming of exploring the globe and experiencing life to its fullest. But at age 78, life seems to have passed him by, until a twist of fate (and a persistent 8-year old Wilderness Explorer named Russell) gives him a new lease on life.",
-    "popularity": 33.890809,
-    "poster_path": "/nk11pvocdb5zbFhX5oq5YiLPYMo.jpg",
-    "production_companies": [
-      {
-        "name": "Pixar Animation Studios",
-        "id": 3
-      }
-    ],
-    "production_countries": [
-      {
-        "iso_3166_1": "US",
-        "name": "United States of America"
-      }
-    ],
-    "release_date": "2009-05-13",
-    "revenue": 735099082,
-    "runtime": 96,
-    "spoken_languages": [
-      {
-        "iso_639_1": "en",
-        "name": "English"
-      }
-    ],
-    "status": "Released",
-    "tagline": "",
-    "title": "Up",
-    "video": false,
-    "vote_average": 7.8,
-    "vote_count": 8327
-  };
 
-  constructor(private http: HttpClient){ }
-
-  getMovies(search: string): Observable<any> {
-    return //this.http.get()
+  constructor(private http: HttpClient) {
   }
 
-  getTestMovie(): Movie{
-    return this.testMovie;
+  getPopularMovies(): Observable<any> {
+    return this.http.get(`${this.movieSearchBaseUrl}popular${this.apiKey}`)
+      .map((response: Response) => {
+        this.movies = response;
+        console.log(this.movies);
+        return this.movies;
+      });
   }
+
+  getMoviesByCompany(companyId: string, releaseYear?: string): Observable<any>{
+    if (releaseYear){
+      return this.http.get(`${this.discoverMoviesBaseUrl}${this.apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_companies=${companyId}&primary_release_year=${releaseYear}`)
+        .map((response: Response) => {
+          this.movies = response;
+          console.log(this.movies);
+          return this.movies;
+        })
+    }
+    else{
+      return this.http.get(`${this.discoverMoviesBaseUrl}${this.apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_companies=${companyId}`)
+        .map((response: Response) => {
+          this.movies = response;
+          console.log(this.movies);
+          return this.movies;
+        })
+    }
+
+  }
+
+  getMoviesBySearch(search: string): Observable<any> {
+    return this.http.get(`${this.movieSearchBaseUrl}${this.apiKey}&language=en-US&query=${search}&page=1&include_adult=false`)
+      .map((response: Response) => {
+        this.movies = response;
+        console.log(this.movies);
+        return this.movies;
+      })
+  }
+
+    getMoviesByGenre(genreId: string, releaseYear?: string){
+      if (releaseYear){
+        return this.http.get(`${this.discoverMoviesBaseUrl}${this.apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=${releaseYear}&with_genres=${genreId}`)
+          .map((response: Response) => {
+            this.movies = response;
+            console.log(this.movies);
+            return this.movies;
+          })
+      }
+      else{
+        return this.http.get(`${this.discoverMoviesBaseUrl}${this.apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genreId}`)
+          .map((response: Response) => {
+            this.movies = response;
+            console.log(this.movies);
+            return this.movies;
+          })
+      }
+    }
+
+  getPosterPath(pathUrl: string){
+    return `${this.moviePosterBaseUrl}${pathUrl}`;
+  }
+
 }

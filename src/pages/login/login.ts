5f1d596cf3ff60561e4823b1f8e278d74bfcac68
Spencer Landon <31264414+smlandon13@users.noreply.github.com>
2018-02-15 08:52:42 -0700

    Commit 1
    LoginPage commit

---
+++ src/pages/login/login.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+// import { AngularFireAuth } from "angularfire2/auth";
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
+
+

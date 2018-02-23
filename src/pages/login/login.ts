5c226acfbf9d35ffa49f7b3779d2e3c8f944aa86
s.landon <smlandon13@me.com>
2018-02-23 09:53:32 -0700

    sick login page yo

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
- // import { NavController } from 'ionic-angular';
+//import { NavController } from 'ionic-angular';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from "firebase/app";
@@ -15,14 +15,17 @@ export class LoginPage {
   loggedIn=false;
   email: string;
   password: string;
+  loginMode: boolean = true;
 
   constructor(private angularFireAuth: AngularFireAuth, private angularFirestore: AngularFirestore){
+  // add 'private navCtrl: NavController'
   }
 
   login(){
     this.angularFireAuth.auth.signInWithEmailAndPassword(this.email, this.password)
     this.angularFirestore.collection('users').add({name: "beep"}).then(data=> {
       console.log(data);
+      //this.navCtrl.pop(LoginPage);
     });
   }
 
@@ -30,14 +33,20 @@ export class LoginPage {
     this.angularFireAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
     this.angularFirestore.collection('users').add({name: "beep"}).then(data=> {
       console.log(data);
+      // this.navCtrl.pop(LoginPage);
     });
   }
   googleLogin(){
     this.angularFireAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
     this.angularFirestore.collection('users').add({name: "beep"}).then(data => {
       console.log(data);
+      // this.navCtrl.pop(LoginPage);
     });
     this.loggedIn = true;
+  }
+
+  toggleLoginMode() {
+    this.loginMode = !this.loginMode;
   }
 
 }
